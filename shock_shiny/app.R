3f422db53595f9e3e4ea43139d1d1b8a3c06cbe2
Aino Ahvo <aino.ahvo@aalto.fi>
2022-05-19 15:27:03 +0300

    crs

--- shock_shiny/app.R
+++ shock_shiny/app.R
@@ -75,6 +75,7 @@ get(load("data/prod_change_countries.RData"))
 get(load("data/NSE_data.RData"))
 
 prod_change_raster <- raster("data/prod_change_raster.tif")
+crs(prod_change_raster) <- 4326
 earthstat_file <- "data/barley_YieldPerHectare.tif"
 
 
@@ -109,7 +110,7 @@ make_shock_raster_shiny <- function(crop_scenarios, shock_column, scenario_numbe
   
   #create raster
   shock_raster <- rasterFromXYZ(raster_data_with_all_cells, res = c(0.08333333, 0.08333333), crs = "+proj=longlat +datum=WGS84", digits = 6)
-  st_crs(shock_raster) <- 4326
+  crs(shock_raster) <- 4326
     
   # #create a vector of shock values
   # shock_vec <- na.omit(c(as.matrix(shock_raster)))
@@ -161,7 +162,7 @@ make_input_raster_shiny <- function(crop_scenarios, shock_column, scenario_numbe
   
   #create raster
   shock_raster <- rasterFromXYZ(raster_data_with_all_cells, res = c(0.08333333, 0.08333333), crs = "+proj=longlat +datum=WGS84", digits = 6)
-  st_crs(shock_raster) <- 4326
+  crs(shock_raster) <- 4326
   
   return(shock_raster)
   
